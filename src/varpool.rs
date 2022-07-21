ee9c21b44087f29fd89806637786ba9190a11680
Liam Eckert <liameckert17@gmail.com>
2022-07-20 22:27:15 -0400

    Added comments to varpool and some necessary funcs for shifts

--- src/varpool.rs
+++ src/varpool.rs
@@ -19,19 +19,23 @@ impl VarPool {
             deref: false,
         }
     }
-
+    //sets the pool to be derefed on the next sweep
     pub fn del(&mut self) {
         self.deref = true
     }
 
     //push a single variable
     pub fn push(&mut self, val: &str, id: &str) {
+        //adds 1 to the last relative address in mem_map or if there are none sets it to 0
         let new_relative = match self.mem_map.len() {
             0 => 0,
             n => self.mem_map[n - 1].get_relative() + 1,
         };
+        //the actual address is the len of the stack
         let new_addr = Address::new(self.stack.len(), new_relative);
+        //push addr to mem_map
         self.mem_map.push(new_addr);
+        //push the corresponding var with the same address to the stack
         self.stack.push(Var::new(val, id, new_addr));
     }
     //push a collection of variables
@@ -43,22 +47,31 @@ impl VarPool {
         let mut new_addr = Address::new(self.stack.len(), new_relative);
         //only adds the first address to the mem map since it is a collection
         self.mem_map.push(new_addr);
+        // does push but without the mem_map push for each var since mem_map only stores the first address of a collection
+        // the virtual machine recognizes gaps in the map as the length of a collection
         for v in collection {
             self.stack.push(v.to_var(new_addr));
             new_relative += 1;
-            new_addr = Address::new(self.stack.len(), new_relative)
+            new_addr = Address::new(self.stack.len(), new_relative);
         }
     }
 
-    pub fn remove_addr(&mut self, addr: Address) {
-        self.stack
-            .retain(|var| var.get_address().get_relative() != addr.get_relative());
-        self.mem_map
-            .retain(|member| member.get_actual() != addr.get_actual());
-        for a in &mut self.mem_map {
-            if a.get_relative() > addr.get_relative() {
-                a.shift_back(1);
-                self.stack[a.get_actual()].shift_address_backward(1);
+    pub fn shift_back_after(&mut self, offset: u8, shift: u8) {
+        // checks if each var's relative address occurs after the offset
+        // if it does occur after the offset the var is shifted back the amount of the shift
+        for v in &mut self.stack {
+            if v.get_address().get_relative() > offset {
+                v.shift_address_backward(shift);
+            }
+        }
+    }
+
+    pub fn shift_forward_after(&mut self, offset: u8, shift: u8) {
+        // checks if the var's relative address occurs after the offset
+        // if it does occur after the offset the var is shifted back the amount of the shift
+        for v in &mut self.stack {
+            if v.get_address().get_relative() > offset {
+                v.shift_address_forward(shift);
             }
         }
     }
