3c806cb625ab9d5181c86ee47685a0f0ec68f3ed
Liam Eckert <liameckert17@gmail.com>
2022-07-25 18:35:55 -0400

    Update cgen.rkt

--- src/cgen.rkt
+++ src/cgen.rkt
@@ -1,23 +1,49 @@
 #lang racket
 
-(define space #\space)  
-(define tab #\tab)
-(define return #\newline)
+(define (to-chars lst)
+  (cond
+    ((null? lst) '())
+    ((string? lst) (string->list lst))
+    ((string? (car lst)) (append
+                          (string->list (car lst))
+                          (to-chars (cdr lst))))
+    ((list? (car lst))
+     (append
+      (to-chars (car lst))
+      (to-chars (cdr lst))))
+    ((number? (car lst))
+     (append
+      (string->list (number->string (car lst)))
+      (to-chars (cdr lst))))
+    ((symbol? (car lst))
+     (append
+      (string->list (symbol->string (car lst)))
+      (to-chars (cdr lst))))
+    (else (cons
+           (car lst)
+           (to-chars (cdr lst))))))
 
-(define-syntax-rule (binary-expr op lhs rhs)
-  (string lhs space op space rhs))
+(define (binary-c-expr lst)
+  (list->string (to-chars lst)))
 
-(define-syntax-rule (add lhs rhs)
-  (binary-expr #\+ lhs rhs))
+(define-syntax-rule (c-add lhs rhs)
+  (binary-c-expr '(lhs #\space + #\space rhs)))
 
-(define-syntax-rule (sub lhs rhs)
-  (binary-expr #\- lhs rhs))
+(define-syntax-rule (c-sub lhs rhs)
+  (binary-c-expr '(lhs #\space - #\space rhs)))
 
-(define-syntax-rule (mult lhs rhs)
-  (binary-expr #\* lhs rhs))
+(define-syntax-rule (c-mult lhs rhs)
+  (binary-c-expr '(lhs #\space * #\space rhs)))
 
-(define-syntax-rule (div lhs rhs)
-  (binary-expr #\/ lhs rhs))
+(define-syntax-rule (c-div lhs rhs)
+  (binary-c-expr '(lhs #\space / #\space rhs)))
 
+(define-syntax-rule (c-equal lhs rhs)
+  (binary-c-expr '(lhs #\space == #\space rhs)))
 
+(define-syntax-rule (c-or lhs rhs)
+  (binary-c-expr '(lhs #\space || #\space rhs)))
+
+(define-syntax-rule (c-and lhs rhs)
+  (binary-c-expr '(lhs #\space && #\space rhs)))
 
